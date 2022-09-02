e34ebe3bc744f52c41e3e2ab573d869aa794ffb3
Edoki Chukwuyem <edokichuks@gmail.com>
2022-09-02 05:42:09 +0100

    model clean up

--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -5,21 +5,8 @@ class Product {
   late final String pDescription;
   late final String pImagePath;
   late final String? pCategory;
-  // late final Map pRating;
-  late final Rating rate;
 
-  /// {
-  /// "id": 2,
-  /// "title": "Mens Casual Premium Slim Fit T-Shirts ",
-  /// "price": 22.3,
-  /// "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-  /// "category": "men's clothing",
-  /// "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-  /// "rating": {
-  /// "rate": 4.1,
-  /// "count": 259
-  /// }
-  /// }
+  late final Rating rate;
 
   Product({
     required this.pId,
@@ -28,7 +15,6 @@ class Product {
     this.pDescription = '',
     required this.pImagePath,
     this.pCategory = '',
-    // required this.pRating,
     required this.rate,
   });
 
@@ -41,7 +27,6 @@ class Product {
     pDescription = json['description'];
     pCategory = json['category'];
     pImagePath = json['image'];
-//    pRating = json['rating'];
 
     rate = Rating.fromJson(json["rating"]);
   }
@@ -54,7 +39,7 @@ class Product {
     data['description'] = pDescription;
     data['category'] = pCategory;
     data['image'] = pImagePath;
-    //  data['rating'] = pRating;
+
     data['rating']['rate'] = rate;
 
     return data;
